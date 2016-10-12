b6ca02d0459b53bc2169a05caa3fc5e222bc2ce0
local <localadmin@fa16-cs241-022.cs.illinois.edu>
2016-10-11 19:26:05 -0500

    lode png working examples

--- test.c
+++ test.c
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-/*
-3 ways to decode a PNG from a file to RGBA pixel data (and 2 in-memory ways).
-*/
 
 /*
 Example 1
@@ -18,11 +15,16 @@ void decodeOneStep(const char* filename)
   std::vector<unsigned char> image; //the raw pixels
   unsigned width, height;
  //decode
-  unsigned error = lodepng::decode(image, width, height, filename);
+  unsigned error = lodepng::decode(image, width, height, filename);  //decode stores pixel data in vector<unsigned char>, using 4 of those for every pixel, one for each channel
   printf("Image Dimensions : %d X %d\n", width, height);
   size_t i = 0;
-  //image++;
-  printf("Whatt : %x\n", image);
+  //Print out the middle chunk.
+  for(i = 500000; i < 510000; i++)
+   {
+     printf("%02x", image[i]);
+     if(i != 0 && i%4 == 0)
+     printf(" ");
+   }
   //if there's an error, display it
   if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
