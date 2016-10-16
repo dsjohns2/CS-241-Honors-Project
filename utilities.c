adf504b87d702c3ddb511d2c77b77c808bd6673a
bershan2 <bershan2@illinois.edu>
2016-10-16 15:33:22 -0500

    Anton Oct 16 utils.c from previous upload
    Forgot to include this file with earlier upload.

--- utilities.c
+++ utilities.c
@@ -20,9 +20,21 @@ char littleEndian(){
 }*/
 
 
+/*
 int main (){
 	if (littleEndian())
 		write (1, "Sysytem is little endian.\n", 26);
 	else	write (1, "Sysytem is big endian.\n", 23);
 	return 0;
+}*/
+
+char * toBin (int n, char * buffer, int bits_per_pix){
+	buffer = (char*)realloc (buffer, sizeof(int)*8+1);
+	buffer[0]=0;
+	for (int c = sizeof(int)*8-1; c >= 0; c--)
+		if ((n>>c) & 1)
+			strcat (buffer, "1");
+		else
+			strcat (buffer, "0");
+	return buffer;
 }
