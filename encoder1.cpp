23a6c135d2e8fb73d728ebf059ec261c85ed1876
bershan2 <bershan2@illinois.edu>
2016-10-16 15:31:31 -0500

    Anton Oct 16 partially fixed fstream encoder/decoder.
    1. The functional bigendian docoder/decoder. Were lightly tested, does not check that print to output stream is successful.
    2. New utility: given int/char returns string that represents int/char in binary.
    3. Some other less important stuff.

--- encoder1.cpp
+++ encoder1.cpp
@@ -10,6 +10,7 @@
 
 #include "lodepng.h"
 #include "encrypt.c"
+#include "utilities.c"
 
 #include <vector>
 #include <stdio.h>
@@ -19,13 +20,17 @@
 #include <assert.h>
 
 #define BITS_PER_PIX 4 // parameter
+#define DEBUG 0
 
 #define CAN_BE_USED ((unsigned)((0b1<<BITS_PER_PIX)-1)) // deffinitely correct
 #define SIGNIFICANT ((unsigned)(~CAN_BE_USED))			// for little endian machines
+#define ENCODE_MASK 0x1000
+
+	char * debug1 = NULL; char * debug2 = NULL;
 
 // 1 if close
 unsigned compare (std::vector<unsigned char> image, unsigned index1, unsigned index2){
-	return (SIGNIFICANT & image[index1] == SIGNIFICANT & image[index2]);
+	return ((SIGNIFICANT & image[index1]) == (SIGNIFICANT & image[index2]));
 }
 
 // Returns 0 if we should not use it, 1 if it's Ok
@@ -35,7 +40,7 @@ unsigned variation (std::vector<unsigned char> image, unsigned index, unsigned w
 	char result = 1;
 	if (   (      line     && compare (image, index, index-width))	// above
 		|| (      column   && compare (image, index, index-1))		// left
-		|| (line<heigh-1   && compare (image, index, index+width))	// below
+		|| (line<height-1   && compare (image, index, index+width))	// below
 		|| (column<width-1 && compare (image, index, index+1)))	// right
 		return 0;
 		else return 1;
@@ -46,7 +51,10 @@ void errorMsg (char * errorMessage){
 	exit (1);
 }
 
+// Big endian
+// Encoder is correct, uncomment the debug stuff to make sure
 void streamEncoder (const char * ballastImage, FILE * secretStream, const char * saveAs){
+		if (DEBUG)	debug1 = NULL; debug2 = NULL;
 	std::vector<unsigned char> image;
 	unsigned width, height, error;
 	error = lodepng::decode (image, width, height, ballastImage);	//Decode png to vector
@@ -58,11 +66,14 @@ void streamEncoder (const char * ballastImage, FILE * secretStream, const char *
 	while (notEOFc && pixelIndex < width*height){
 		if (character==EOF)	// exactly one EOF is printed
 			notEOFc=0;
-		//printf ("Enc char %c\n", character);
+				if (DEBUG){ debug1=toBin(character, debug1, BITS_PER_PIX); printf ("Echar %c %s\n", character, debug1);}
 		for (unsigned bitsDone=0; bitsDone<sizeof(int)*8/*bits per byte*/; bitsDone+=BITS_PER_PIX){
 			unsigned newPixel = (SIGNIFICANT & image[pixelIndex]) | (CAN_BE_USED & character);
+			//unsigned newPixel = (SIGNIFICANT & image[pixelIndex]) | (ENCODE_MASK & character);
+					if (DEBUG){ debug1 = toBin (image[pixelIndex], debug1, BITS_PER_PIX); debug2 = toBin (newPixel, debug2, BITS_PER_PIX);
+							printf ("   ---pixel change---\n   from %s\n    to  %s\n", debug1, debug2);}
 			image[pixelIndex] = newPixel;
-			character >>= BITS_PER_PIX;
+			character >>= BITS_PER_PIX;//<<
 			pixelIndex++;
 		}
 		character = fgetc (secretStream);
@@ -70,15 +81,46 @@ void streamEncoder (const char * ballastImage, FILE * secretStream, const char *
 
 	//Turn vector back into png
 	error = lodepng::encode (saveAs, image, width, height); 
+		if (DEBUG) {free (debug1);free(debug2);}
 }
 
+// Correctly decodes package Image but can not write to the output stream
 void streamDecoder (const char * packageImage, FILE * secretStream){
+			if (DEBUG) debug1 = NULL;
 	std::vector<unsigned char> image;
 	unsigned width, height, error;
 	error = lodepng::decode (image, width, height, packageImage);	//Decode png to vector
 
 	unsigned pixelIndex = 0;
-	int character  = 1;
+	int character;
+	while (pixelIndex < width*height){
+		character = 0;
+		for (int bitsDone=0; bitsDone<sizeof(int)*8/*bits per byte*/; bitsDone+=BITS_PER_PIX){
+			int newBits = image[pixelIndex] & CAN_BE_USED;
+			character = character | (newBits<<bitsDone);
+				if (DEBUG) {debug1 = toBin (character, debug1, BITS_PER_PIX); printf ("Dchar %c = %s\n", character, debug1);}
+			pixelIndex++;
+		}
+		if (character==EOF) break;
+		// !!! UNCOMMENT ME AND TRY TO FIX !!!
+		fputc(character, secretStream);
+//		if (fputc(character, secretStream))
+//			errorMsg ((char*)"Decoder: Could not write to file.");
+				if (1) {debug1 = toBin (character, debug1, BITS_PER_PIX);printf ("Got char %c = %s\n", character, debug1); }
+	}
+			free(debug1);
+}
+
+// I think it is little endin, but it could just be buggy big endian 
+// I'm not sure (Anton)
+void streamDecoder1 (const char * packageImage, FILE * secretStream){
+			if (DEBUG) debug1 = NULL;
+	std::vector<unsigned char> image;
+	unsigned width, height, error;
+	error = lodepng::decode (image, width, height, packageImage);	//Decode png to vector
+
+	unsigned pixelIndex = 0;
+	int character;
 	while (pixelIndex < width*height){
 		character = 0;
 		for (int bitsDone=0; bitsDone<sizeof(int)*8/*bits per byte*/; bitsDone+=BITS_PER_PIX){
@@ -91,7 +133,9 @@ void streamDecoder (const char * packageImage, FILE * secretStream){
 			printf ("Bad char '%c'=%i", character, (int)character);
 			errorMsg ((char*)"Decoder: Could not write to file.");
 		}
+				if (DEBUG) { debug1 = toBin (character, debug1, BITS_PER_PIX);printf ("Got char %c = %s\n", character, debug1);}
 	}
+			if (DEBUG) free(debug1);
 }
 
 char * inputPrompt (char * message){
@@ -107,6 +151,22 @@ char * inputPrompt (char * message){
 	}
 }
 
+// unfinished function
+void dump_and_compare (FILE * one, FILE * two){
+	printf ("Comparing two files:\n");
+	int char1 = getc (one);
+	int char2 = getc (two);
+			char * c1 = NULL; char * c2 = NULL;
+	while (char1!=EOF && char2!=EOF){
+				c1 = toBin (char1, c1, BITS_PER_PIX);
+				c2 = toBin (char2, c2, BITS_PER_PIX);
+		printf ("--------------------\n %c = %s\n %c = %s\n", char1, c1, char2, c2);
+
+		char1 = getc (one); char2 = getc (two);
+	}
+			free (c1); free(c2);
+}
+
 int main(int argc, char *argv[]){
 	assert (CAN_BE_USED + SIGNIFICANT == (unsigned)(~(unsigned)0));
 	assert ((CAN_BE_USED & SIGNIFICANT) == 0);
@@ -119,11 +179,13 @@ int main(int argc, char *argv[]){
 	streamDecoder ("output2.png", outSecretStream);
 	fclose (outSecretStream);
 	
-//	// compare files
-//	FILE * original = fopen ("secretMessage.txt", "r");
-//	FILE * result   = fopen ("~secretMessage.txt", "r");
-//	fclose (original);
-//	fclose (result);
-	
+/*
+	//	compare files
+	FILE * original = fopen ("secretMessage.txt", "r");
+	FILE * result   = fopen ("~secretMessage.txt", "r");
+	dump_and_compare (original, result);
+	fclose (original);
+	fclose (result);
+*/	
 	return 0;
 }
